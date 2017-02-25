a373c456f3f104d2bd8c8a9fd4fa70ca2ccf3eb8
Shaun Martin <shaun@samsite.ca>
2017-02-25 11:42:45 -0800

    first draft of env var config

--- elasticpypi/config.py
+++ elasticpypi/config.py
@@ -1,7 +1,3 @@
-import json
 import os
 
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-with open('{}/config.json'.format(dir_path), 'r') as f:
-    config = json.load(f)
+config = { k: v for k,v in os.environ.iteritems() }
