ad3d4ebf1e63905b03c2061e2b19846bd808a208
Shaun Martin <shaun@samsite.ca>
2017-03-04 10:29:31 -0800

    wrote local docker tests while travis was running, fixed true/false vals in env var configs

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:2
+MAINTAINER Shaun Martin <shaun@samsite.ca>
+
+VOLUME /code
+
+ADD requirements.txt /tmp/
+ADD test-requirements.txt /tmp/
+
+RUN pip install -r /tmp/requirements.txt && \
+    pip install -r /tmp/test-requirements.txt
+
+CMD cd /code && python -m pytest
--- elasticpypi/config.py
+++ elasticpypi/config.py
@@ -9,4 +9,18 @@ WATCHED=[
     'PASSWORD',
     'OVERWRITE'
 ]
+
+TRUTHY = ['TRUE', 'true', 1]
+FALSEY = ['FALSE', 'false', 0]
+
+# load config
 config = {k.lower(): v for k, v in os.environ.iteritems() if k in WATCHED}
+
+# check config
+for k, v in config.iteritems():
+    # assume env vars are always stringy
+    if v in TRUTHY:
+        config[k] = True
+    if v in FALSEY:
+        config[k] = False
+
