a5773bc10cff33e9e740c57cd587ee1ff15f92ef
Kyle Hornberg <kyle.hornberg@gmail.com>
2017-12-27 09:33:37 -0600

    Feature: Upload files

--- elasticpypi/api.py
+++ elasticpypi/api.py
@@ -2,6 +2,8 @@ import boto3
 from flask import Flask
 from flask import send_file
 from flask import render_template
+from flask import request
+from flask import abort
 from elasticpypi.auth import requires_auth
 from elasticpypi import s3, dynamodb
 from elasticpypi.config import config
@@ -9,9 +11,18 @@ from elasticpypi.config import config
 app = Flask(__name__)
 
 
-@app.route("/simple/")
+@app.route('/simple/', methods=['GET', 'POST'])
 @requires_auth
 def simple():
+    if request.method == 'POST':
+        f = request.files['content']
+        if '/' in f.filename:
+            abort(400)
+        print(config)
+        if config['overwrite'] == 'false' and s3.exists(f.filename):
+            abort(409)
+        s3.upload(f.filename, f.stream)
+        return '', 200
     db = boto3.resource('dynamodb')
     prefixes = dynamodb.list_packages(db)
     return render_template('simple.html', prefixes=prefixes, stage=config['stage'])
