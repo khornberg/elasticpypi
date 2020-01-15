c5e9c0f6ff7eb22ea13abb2a9bb1feebaf529eeb
Vlad Emelianov <volshebnyi@gmail.com>
2020-01-15 23:01:48 +0100

    Fix version extraction

--- elasticpypi/name.py
+++ elasticpypi/name.py
@@ -1,15 +1,19 @@
-"""
-From https://github.com/vendasta/cloudpypi/blob/master/cloudpypi/package_api.py#L14
-"""
 import re
 
-RE_NORMALIZE = re.compile(r"[-_.]+")
+RE_PACKAGE_NAME = re.compile(r"^(?P<name>.+)-(?P<version>\d+(\.\d+[^-.]*)+)")
 
 
 def compute_version(package_name: str) -> str:
-    return package_name.split("-", 2)[1]
+    match = RE_PACKAGE_NAME.match(package_name)
+    if not match:
+        raise ValueError(f"Wrong package name format: {package_name}")
+
+    return match.group("version")
 
 
 def normalize(package_name: str) -> str:
-    package_title = package_name.split("-", 1)[0]
-    return RE_NORMALIZE.sub("-", package_title)
+    match = RE_PACKAGE_NAME.match(package_name)
+    if not match:
+        raise ValueError(f"Wrong package name format: {package_name}")
+
+    return match.group("name")
