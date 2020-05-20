c834759c51a68a410628b7a6e1fb0f723566770d
Vlad Emelianov <volshebnyi@gmail.com>
2020-05-20 03:25:03 +0200

    Remove stage, add new route to config

--- elasticpypi/api.py
+++ elasticpypi/api.py
@@ -15,9 +15,7 @@ def simple() -> str:
     dynamodb_client = DynamoDBClient(env_namespace.table)
     normalized_names = dynamodb_client.list_normalized_names()
     normalized_names.sort()
-    return render_template(
-        "simple.html", normalized_names=normalized_names, stage=env_namespace.stage
-    )
+    return render_template("simple.html", normalized_names=normalized_names)
 
 
 @app.route("/simple/<normalized_name>/")
@@ -29,14 +27,11 @@ def simple_name(normalized_name: str) -> str:
     if not packages:
         abort(404)
     return render_template(
-        "links.html",
-        packages=packages,
-        normalized_name=normalized_name,
-        stage=env_namespace.stage,
+        "links.html", packages=packages, normalized_name=normalized_name
     )
 
 
-@app.route("/download/<package_name>/")
+@app.route("/simple/download/<package_name>")
 def download(package_name: str) -> str:
     env_namespace = EnvNamespace(os.environ)
     s3_client = S3Client(env_namespace.bucket)
